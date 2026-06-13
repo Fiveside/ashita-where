c50b701c9aaea5317abb90c97d66bc47ab6a6342
Fiveside <ErichRHealy@gmail.com>
2026-06-12 23:12:08 -0700

    stub double metaphone

---
+++ fuzzy/double_metaphone.lua
@@ -0,0 +1,10 @@
+
+---An implementation of the double metaphone algorithm in pure lua
+---@param input string The string to transform
+---@return {[1]: string, [2]: string}
+function doubleMetaphone(input)
+    -- To be implemented.
+    return {"", ""};
+end
+
+return doubleMetaphone;
